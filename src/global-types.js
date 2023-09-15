197a38fba52dcd0c6e9091b0aeda984d66a7e402
Parth  Panjwani <parth.panjwani109038@marwadiuniversity.ac.in>
2023-09-15 23:50:46 +0530

    compilation ready

--- src/global-types.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var googlePlayScraper = require("google-play-scraper");
-var appInfo = googlePlayScraper.app({ appId: "com.sarrthi.courses" });
